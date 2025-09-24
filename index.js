adfbad4c5f5136444d84a723fa9596efe8862923
aalvarez351 <aalvarez351@gmail.com>
2025-09-24 12:48:54 -0500

    x

--- index.js
+++ index.js
@@ -7,10 +7,30 @@ const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 
 const app = express();
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://aalvarez351.github.io', 'https://conectandopersonas.life'],
-  credentials: true
-}));
+// CORS configuration
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'https://aalvarez351.github.io',
+    'https://conectandopersonas.life',
+    'https://clean-daphene-personal351-7963be99.koyeb.app'
+  ];
+
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin) || origin?.match(/\.koyeb\.app$/)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 app.use(express.json());
 app.use(express.static('.'));
 
